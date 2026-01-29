5e5aa3e7586f32242d060037e7784fef7241cfb2
animestopbus-ctrl <animestopbus@gmail.com>
2026-01-29 20:20:28 +0530

    Update __init__.py

--- Rexbots/__init__.py
+++ Rexbots/__init__.py
@@ -2,13 +2,13 @@
 # Don't Remove Credit
 # Telegram Channel @RexBots_Official
 
-
-# Rexbots
-# Don't Remove Credit
-# Telegram Channel @RexBots_Official
-
-
-
-# Rexbots
-# Don't Remove Credit
-# Telegram Channel @RexBots_Official
+from .admin import *
+from .broadcast import *
+from .caption import *
+from .premium import *
+from .session import *
+from .settings import *
+from .start import *
+from .thumbnail import *
+from .words import *
+from .bot import *  # Interconnect save handler
